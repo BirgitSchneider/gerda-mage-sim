6beef19ec1167bbf4c6e81945288ed553161354b
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2017-11-14 10:20:47 +0100

    update: Tue Nov 14 10:20:47 CET 2017

--- github-sync.sh
+++ github-sync.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 while true; do
-  cd /nfs/gerda5/gerda-simulations/gerda-mc2
+  cd /nfs/gerda5/var/gerda-simulations/gerda-mc2
   if git diff --quiet; then
+    sleep 86400
+  else
     echo "Changes found, committing and pushing to github.com..."
     git pull --quiet
     git add .
     git commit --quiet -m "update: $(date)"
     git push --quiet
-  else
-    sleep 86400
   fi
 done
