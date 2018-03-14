7180ef54c2fd374fe41dbfe68522aa1a326d35b7
Luigi Pertoldi <luigi.pertoldi@pd.infn.it>
2018-03-14 23:41:20 +0100

    now it does everything, but it's ugly

--- UTILS/post/latex-report.jl
+++ UTILS/post/latex-report.jl
@@ -1,11 +1,11 @@
-#= report-latex.jl
+#= latex-report.jl
  =
  = Author: Luigi Pertoldi - pertoldi@pd.infn.it
  = Created: 14 Mar 2018
  =#
 using JSON
 
-(length(ARGS) == 0) && (println("USAGE: julia report-latex.jl [sim-parameters-all.json]"); exit())
+(length(ARGS) == 0) && (println("USAGE: julia latex-report.jl [sim-parameters-all.json]"); exit())
 const sims = JSON.parsefile(ARGS[1])
 
 preamble =
@@ -69,7 +69,6 @@ closing =
             \bottomrule
         \end{tabular}
     \end{table}
-    \end{document}
     """
 
 body = ""
@@ -144,15 +143,127 @@ end
 
 massestable = heading * body * closing
 
-#================================= TABLE WITH PRIMARIES ====================================#
-
-
-
-#================================= WRITE AND COMPILE =======================================#
-
+# write
 open("/tmp/gerda-mage-sim-report.tex", "w") do f
     write(f, preamble * list * '\n' * massestable)
 end
+
+#================================= TABLE WITH PRIMARIES ====================================#
+
+# let's do a table for each volume
+for vol in all_keys
+    (vol == "general") && continue
+
+    parts = copy(sims[vol])
+    # delete non-parts from parts array
+    for (p,val) in sims[vol]
+        if !isa(val, Dict)
+            delete!(parts, p)
+        elseif (!haskey(val, "mc-volume-cm3") &&
+                !haskey(val, "mc-surface-cm2"))
+            delete!(parts, p)
+        end
+    end
+
+    # get all the isotopes in that volume
+    iso_all = []
+    for p in collect(keys(parts))
+        for is in collect(keys(sims[vol][p]))
+            if (is != "mc-mass-kg"    && is != "mc-density-kgcm3" &&
+                is != "mc-volume-cm3" && is != "mc-surface-cm2" && is != "mean-enrichment-fraction")
+                push!(iso_all, is)
+            end
+        end
+    end
+
+    iso = unique(iso_all)
+    sort!(iso)
+
+    c_c = "";
+    for _ in iso
+        c_c = c_c * 'c'
+    end
+
+    # in LaTeX you have to escape _ characters
+    v = replace("\\texttt{$vol}", "_", "\\_")
+    heading =
+        """
+        \\begin{table}
+            \\centering
+            \\caption{Number of simulated events for each isotope in the $v \\textsc{Gerda} volume.}
+            \\begin{tabular}{l$c_c}
+                \\toprule
+                part & \\texttt{$(join(iso, "} & \\texttt{"))} \\\\
+                \\midrule
+        """
+
+    closing =
+        raw"""
+                \bottomrule
+            \end{tabular}
+        \end{table}
+        """
+
+    body = ""
+
+    for (p,val) in parts
+        # in LaTeX you have to escape _ characters
+        p = replace("\\texttt{$p}", "_", "\\_")
+        # indent
+        body = body * "        "
+        body = body * @sprintf("%-30s", p) * " & "
+        for i in iso
+            if (haskey(val, i) &&
+                haskey(val[i], "edep") &&
+                haskey(val[i]["edep"], "primaries"))
+                body = "$body $(val[i]["edep"]["primaries"])"
+            else
+                body = body * "      "
+            end
+            i != iso[end] ? (body = body * " & ") : (body = body * " \\\\\n")
+        end
+        # indent
+        body = body * "        "
+        body = body * @sprintf("%-30s", ' ') * " & "
+        for i in iso
+            if (haskey(val, i) &&
+                haskey(val[i], "coin") &&
+                haskey(val[i]["coin"], "primaries"))
+                body = "$body $(val[i]["coin"]["primaries"])"
+            else
+                body = body * "      "
+            end
+            i != iso[end] ? (body = body * " & ") : (body = body * " \\\\\n")
+        end
+    end
+    if (haskey(sims[vol], "mc-mass-kg") ||
+        haskey(sims[vol], "mc-volume-cm3") ||
+        haskey(sims[vol], "mc-density-kgcm3"))
+        # write first two columns
+        body = body * "        "
+        body = body * @sprintf("%-30s &", "\\textsc{total}")
+        for i in iso
+            body = "$body $(haskey(sims[vol][i], "edep") ? sims[vol][i]["edep"]["primaries"] : "    ")"
+            i != iso[end] ? (body = body * " & ") : (body = body * " \\\\\n")
+        end
+        body = body * "        "
+        body = body * @sprintf("%-30s &", ' ')
+        for i in iso
+            body = "$body $(haskey(sims[vol][i], "coin") ? sims[vol][i]["coin"]["primaries"] : "   ")"
+            i != iso[end] ? (body = body * " & ") : (body = body * " \\\\\n")
+        end
+    end
+
+    # write-append
+    open("/tmp/gerda-mage-sim-report.tex", "a") do f
+        write(f, '\n' * heading * body * closing)
+    end
+end
+
+open("/tmp/gerda-mage-sim-report.tex", "a") do f
+    write(f, "\n\\end{document}")
+end
+#====================================== COMPILE ============================================#
 
 try
     readchomp(`which pdflatex`)
