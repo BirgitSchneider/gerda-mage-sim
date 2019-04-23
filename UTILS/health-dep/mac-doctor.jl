d750c0503358640d290039bfd0458990c9abdb51
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2019-04-23 18:34:52 +0200

    [health-dep] improve mac-doctor and travis file

--- UTILS/health-dep/mac-doctor.jl
+++ UTILS/health-dep/mac-doctor.jl
@@ -5,99 +5,103 @@
  =#
 using JSON
 
-# check arguments
-(length(ARGS) < 1) && error("USAGE: julia mac-doctor.jl <macro-file> [...]")
+function check_macro(mac)
 
-exit_code = true
-for fn in ARGS
+    myerror(str) = println("$mac: $str")
+
+    ecode = 0
     # check file existence
-    mac = fn
     open(mac) do f
         global lines = readlines(f)
     end
 
-    # build up database of fields from sim-parameters-all.json
-    sim_par = JSON.parsefile(@__DIR__() * "/../sim-parameters-all.json")
-    db = []
-    for (vol,v1) in sim_par
-        (vol == "general") && continue
-        push!(db, vol)
-        for (part,v2) in v1
-            push!(db, part)
-            for (iso,v3) in v2
-                !occursin(iso, "mc-") && push!(db, iso)
-            end
-        end
-    end
-    push!(db, "coin", "edep")
-    db = unique(db)
-
     # function to check for line in macro
-    function check_line(lines, command, value)
+    function check_line(lines, command, values; ispath=false)
         index = findfirst(x -> occursin(Regex(command), x), lines)
-        words = split(lines[index], ['\t',' '], keepempty = false)
-
-        if (index == 0)
-            println("ERROR: $command command not found in macro!")
-            global exit_code = false
+        if (index == nothing)
+            myerror("$command command not found in macro!")
+            ecode = 1
             return
         end
 
+        words = split(lines[index], ['\t',' '], keepempty = false)
+
         if (length(words) > 2)
-            println("ERROR: too much words at line " * index)
-            global exit_code = false
+            myerror("too much words at line " * index)
+            ecode = 1
         end
 
-        if (words[2] != value)
-            println("ERROR: wrong value of $command")
-            global exit_code = false
+        for v in values
+            if ispath
+                relpath(String(words[2])) == relpath(v) && return
+            else
+                words[2] == v && return
+            end
         end
+
+        myerror("wrong value of $command")
+        ecode = 1
     end
 
     # if it's a regular macro
-    if occursin(r"raw-\w+-\w+-\w+-\w+-\w+\.mac", basename(mac))
+    if occursin(r"raw-.+-.+-.+-.+\.mac", basename(mac))
 
         # extract fields
         fields = split(basename(mac), ['-', '.'])
 
-        for i in [2:5]
-            # check for presence of fields in database
-            if (fields[i] in db)
-                println("ERROR: $(fields[i]) not found in sim-parameters-all.json!")
-                global exit_code = false
-            end
-        end
-
         # check if path matches name
         if !occursin("/$(fields[2])/$(fields[3])/$(fields[4])/$(fields[5])/log/$(basename(mac))", abspath(mac))
-            println("ERROR: macro name does not match macro path")
-            global exit_code = false
+            myerror("macro name does not match macro path")
+            ecode = 1
         end
 
         # check if preamble is correctly loaded
         check_line(lines,
-                   "/control/execute",
-                   "./$(fields[2])/$(fields[3])/$(fields[4])/$(fields[5])/log/preamble.mac")
+            "/control/execute",
+            ["./$(fields[2])/$(fields[3])/$(fields[4])/$(fields[5])/log/preamble.mac"],
+            ispath=true)
 
         # check root filename
         check_line(lines,
-                    "/MG/eventaction/rootfilename",
-                    "./$(fields[2])/$(fields[3])/$(fields[4])/$(fields[5])/$(splitext(basename(mac))[1]).root")
+            "/MG/eventaction/rootfilename",
+            ["./$(fields[2])/$(fields[3])/$(fields[4])/$(fields[5])/$(splitext(basename(mac))[1]).root"],
+            ispath=true)
 
     # if it's a preamble
     elseif (basename(mac) == "preamble.mac")
+        @debug "$mac is a preamble macro"
         check_line(lines,
                     "/MG/geometry/detector/geometryfile",
-                    "geometry_PhaseII_BEGeFCCDgrowth_CoaxPhaseI.dat")
+                    ["geometry_PhaseII_BEGeFCCDgrowth_CoaxPhaseI.dat", "geometry.json"])
         check_line(lines,
                     "/MG/geometry/detector/matrixfile",
-                    "matrix_phase_ii_StatusDec2015_pos.dat")
+                    ["matrix_phase_ii_StatusDec2015_pos.dat", "matrix-PhaseII-dec15.json"])
     else
-        println("ERROR: $fn -> Extraneous file or not in the form raw-<volume>-<part>-<isotope>-<type>-###.mac / preamble.mac")
-        global exit_code = false
+        myerror("extraneous file or not in the form raw-<volume>-<part>-<isotope>-<type>-###.mac / preamble.mac")
+        ecode = 1
+    end
+
+    return ecode
+end
+
+ ##########
+## ROUTINE ##
+ ##########
+
+gerda_ms = rstrip(abspath("$(@__DIR__)/../.."), '/')
+cd(gerda_ms)
+
+exit_code = 0
+
+for (root, dirs, files) in walkdir(gerda_ms)
+    occursin("$gerda_ms/alphas", root) && continue
+    for mac in filter(x -> occursin(r"(raw-.+|preamble)\.mac", x), files)
+        root = replace(root, "$gerda_ms/" => "")
+        @debug "analyzing $root/$mac"
+        global exit_code += check_macro("$root/$mac")
     end
 end
 
-if (!exit_code)
+if exit_code != 0
     error("Errors occurred! Check your macros")
 end
